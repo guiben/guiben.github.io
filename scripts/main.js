a8e699b16446d802d6c53fb8a801a54fffb05f3b
guiben <benjamin.guiben.guiller@gmail.com>
2014-08-20 20:50:47 +0200

    Update adding ministyle + style

--- scripts/main.js
+++ scripts/main.js
@@ -6,13 +6,35 @@ function loaded(){
   var cvcontent   = document.getElementById("cv");
 
   var toggle = function(enable, disable){
-    enable.map(function(e){ e.style.visibility = "visible"; e.style.display = ""; });
-    disable.map(function(e){ e.style.display = "none" });
+    enable.map(function(e){
+      e.style.visibility = "visible";
+      e.style.display = ""; 
+    });
+    disable.map(function(e){
+      e.style.display = "none" 
+    });
   };
 
-  cvbutton.addEventListener("click", function(){ toggle([cvcontent], [homecontent]); }); 
-  homebutton.addEventListener("click", function(){ toggle([homecontent], [cvcontent]); });
+  var updatebuttons = function(enable, disable){
+    enable.map(function(e){
+      e.style.borderBottom = "solid 1px rgb(207, 58, 140)";
+    });
+    disable.map(function(e){
+      e.style.borderBottom = "";
+    });
+  };
+
+  cvbutton.addEventListener("click", function(){ 
+    toggle([cvcontent], [homecontent]);
+    updatebuttons([cvbutton], [homebutton]);
+  }); 
+
+  homebutton.addEventListener("click", function(){
+    toggle([homecontent], [cvcontent]);
+    updatebuttons([homebutton], [cvbutton]);
+  });
   cvcontent.style.display = "none";
+  homebutton.style.borderBottom = "solid 1px rgb(207, 58, 140)";
 }
 
 document.addEventListener('DOMContentLoaded', loaded, false);
