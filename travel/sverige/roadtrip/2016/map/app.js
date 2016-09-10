3055792e2ff4caf4ea986a7d3f67f5e54953489c
guiben <benjamin.guiben.guiller@gmail.com>
2016-09-10 13:16:49 +0200

    Sverige 2016 map

---
+++ travel/sverige/roadtrip/2016/map/app.js
@@ -0,0 +1,41 @@
+var mymap = L.map('mapid').setView([63, 12], 4);
+
+L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+		attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(mymap);
+
+function onEachFeature(feature, layer) {
+	if(feature.properties && feature.properties.name) {
+		layer.bindPopup(feature.properties.popupContent);
+	}
+}
+
+var geojsonMarkerOptions = {
+	radius: 4,
+	fillColor: "#ff7800",
+	color: "#ff7f00",
+	weight: 1,
+	opacity: 1,
+	fillOpacity: 0.8
+};
+
+L.geoJson(geojsonData, {
+	onEachFeature: onEachFeature,
+	pointToLayer: function (feature, latlng) {
+		return L.circleMarker(latlng, geojsonMarkerOptions);
+	}
+}).addTo(mymap);
+
+var myStyle = {
+    "color": "#ff7800",
+    "weight": 5,
+    "opacity": 0.65
+};
+
+L.geoJson(myLines, {
+	style: myStyle
+}).addTo(mymap);
+
+mymap.addEventListener("click", function(evt){
+	console.log(evt.latlng);
+});
